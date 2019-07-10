556cd2fbf750bbe4d7d40e73730419423eda51ac
ppbra <kaka020998@gmail.com>
2019-07-10 18:23:21 +0700

    update

---
+++ condition.rb
@@ -0,0 +1,32 @@
+x = 1
+# if x <1 and x > 0 
+#     puts "a"
+#     puts "b"
+# elsif x > 1 and x == 2
+#     puts "c"
+#     puts "d"
+# end
+
+
+# unless x == 1 
+#     puts "x!=1"
+# else
+#     puts "x=1"
+# end 
+
+
+
+my_case = 6
+
+case my_case 
+    when 1
+        puts "1"
+    when 2
+        puts "2"
+    when 3
+        puts "3"
+    when 4
+        puts "4"
+    else
+        puts "default"
+end
---
+++ hash.rb
@@ -0,0 +1,19 @@
+# A Hash is a collection of key-value pairs
+# Mảng băm là 1 tập hợp căc cặp giá trị key-value
+
+# It is similar to an Array, except that indexing is done via arbitrary keys of any
+# object type, not an integer index
+# Tương tự như array , chỉ khác là đánh  chỉ só là một giá trị ngâũ nhiện , không phải là số nguyên 
+
+# Hashes enumerate their values in the order that the corresponding keys were
+# inserted
+# Mảng băm liệt kê các giá trị theo các giá trị khóa tương ứng được chèn
+
+# Hashes have a default value that is returned when accessing keys that do not
+# exist in the hash. By default, that value is nil
+#Mảng băng có giá trị ngẫu nhiên là nil , trả về nếu khóa để  tìm kiêm không match với giá trị nào
+
+puts h = Hash.new
+
+
+puts h1  = Hash.new
