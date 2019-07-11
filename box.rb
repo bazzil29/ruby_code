1bc6276836ee40e8497b4795693a8cdf84ff440c
ppbra <kaka020998@gmail.com>
2019-07-11 15:29:48 +0700

    method , variables of class

--- box.rb
+++ box.rb
@@ -23,6 +23,16 @@ class Box
     def getArea
         return @width*@height
     end
+
+
+    def self.hello (something)
+        @@something = something
+        if something
+            puts something.to_s + " Hello world!"
+        else
+            puts "Notthing to say hello!"
+        end
+    end
 end
 
 x = Box.new(10,20)
@@ -34,7 +44,11 @@ puts "This is height of the box " + x.printHeight().to_s
 
 x.setHeight(100)
 x.setWidth(10)
- puts "This is width of the box " + x.printWidth.to_s
- puts "This is height of the box" + x.printHeight.to_s
+puts "This is width of the box " + x.printWidth.to_s
+puts "This is height of the box" + x.printHeight.to_s
 
 puts  "The area of this bos is " + x.getArea.to_s
+
+Box.hello("hihi")
+
+
